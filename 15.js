cf1e0ffdd0802ff83529c7b22334d3ba9e2f0ee5
OchirovBair <pactuc@gmail.com>
2023-11-19 14:06:23 +0800

    feat: add #15 solution

---
+++ 15.js
@@ -0,0 +1,15 @@
+// TITLE: Third Angle of a Triangle
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+// SOLUTION:
+
+function otherAngle(a, b) {
+  let c = 180 - a - b;
+  return c;
+}
